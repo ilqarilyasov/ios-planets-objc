b3026cbd8d1a45c8f74b1d7e75c424a6f120d6e0
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-11-12 15:39:12 -0600

    LSSettingsVC scene completed. Outlets/actions added

---
+++ Planets Objc/Planets Objc/View Controller/LSSettingsViewController.m
@@ -0,0 +1,38 @@
+//
+//  LSSettingsViewController.m
+//  Planets Objc
+//
+//  Created by Ilgar Ilyasov on 11/12/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+#import "LSSettingsViewController.h"
+
+@interface LSSettingsViewController ()
+
+@end
+
+@implementation LSSettingsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)doneTapped:(UIButton *)sender {
+}
+
+- (IBAction)showPlutoSwithcTapped:(UISwitch *)sender {
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
